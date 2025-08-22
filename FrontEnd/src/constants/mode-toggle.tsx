31630ffe2860ee8dcdebe8cc48a3c6f38df83361
Clementwa0 <clementwa01@gmail.com>
2025-08-23 00:14:03 +0300

    Update Theme Toggle Mode

--- FrontEnd/src/constants/mode-toggle.tsx
+++ FrontEnd/src/constants/mode-toggle.tsx
@@ -21,7 +21,7 @@ export function ModeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="dark:bg-gray-900 ">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
