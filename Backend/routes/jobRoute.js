126ed547c127270af050e8a99ebf8e3b44d83f1f
Clementwa0 <clementwa01@gmail.com>
2025-08-26 22:51:20 +0300

    feat(job-routes): add routes with Multer file upload support and auth middleware

--- Backend/routes/jobRoute.js
+++ Backend/routes/jobRoute.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const upload = require("../config/multer");
 const { addJob, getJobs, getJobById, updateJob, deleteJob } = require('../controllers/jobControllers');
 const router = express.Router();
 const auth = require('../middleware/auth');
 
 
-router.post('/', auth, addJob);
+router.post(
+  "/",
+  upload.fields([
+    { name: "resumeFile", maxCount: 1 },
+    { name: "coverLetterFile", maxCount: 1 },
+  ]),
+  addJob
+);
 router.get('/', auth, getJobs);
 router.get('/:id', auth, getJobById);
 router.put('/:id', auth, updateJob);
