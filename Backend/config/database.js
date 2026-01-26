8fbfa9dd9d524c741991cae2f8e81a16ec70f199
Clement Wambua <109575147+Clementwa0@users.noreply.github.com>
2026-01-26 21:13:19 +0300

    Refactor database connection to use environment variable
    Updated MongoDB connection to use environment variable for URI.

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jobtrailapp_db_user:z62htyTIpygya7yc@cluster0.bscissx.mongodb.net/jobtrail?appName=Cluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Database connection error:', error);
@@ -14,4 +10,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB; 
+module.exports = connectDB;
