5238f7053e4f188fa7ea60e764d1942157cade50
ClementWa0 <clementwa01@gmail.com>
2026-04-16 12:51:42 +0300

    refactor: update app bootstrap and providers

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -5,11 +5,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { HelmetProvider } from "react-helmet-async";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
-      <Toaster position="top-right" richColors />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster position="top-right" richColors />
+      </QueryClientProvider>
     </HelmetProvider>
   </StrictMode>,
 );
