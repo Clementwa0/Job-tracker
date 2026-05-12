205dbba306a0e4108f010f1c1888204a035760c0
ClementWa0 <clementwa01@gmail.com>
2026-05-12 18:11:11 +0300

    Update Frontend/src/hooks/useUpdateJob.ts

--- Frontend/src/hooks/useUpdateJob.ts
+++ Frontend/src/hooks/useUpdateJob.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { updateJob } from "@/features/jobs/api/jobs-api";
+import { updateJob } from "@/services/jobs.service";
 
 export const useUpdateJob = () => {
   const queryClient = useQueryClient();
