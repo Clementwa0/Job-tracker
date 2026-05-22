a3b9e9c3089582b2588631f5b96bd61d6df7640f
ClementWa0 <clementwa01@gmail.com>
2026-05-22 23:54:40 +0300

    refactor: improve register page layout

--- Frontend/src/pages/Auth/Register.tsx
+++ Frontend/src/pages/Auth/Register.tsx
@@ -1,4 +1,7 @@
 import { RegisterFeatures, RegisterForm } from "@/components";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
@@ -7,6 +10,14 @@ const Register = () => {
       <RegisterFeatures />
 
       {/* Right Panel */}
+      <div className="absolute flex flex-end top-4 right-4 ">
+        <Button asChild variant="default" size="sm" className="px-2 py-1 text-gray-100 mb-4">
+          <Link to="/" className="flex items-center gap-1  ">
+          <ArrowLeft className="w-3 h-3" />
+            Back to Home
+          </Link>
+        </Button>
+      </div>
       <div className="flex-1 flex items-center justify-center p-4 lg:p-6 bg-animated relative overflow-hidden">
         <RegisterForm />
       </div>
